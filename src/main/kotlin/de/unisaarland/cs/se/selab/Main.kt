7ac1b630e33791d44b688174205f21d0ce7a2cc1
skysw <skwa00001@stud.uni-saarland.de>
2024-09-11 21:04:44 +0200

    Added main, still needs to run Simulator

--- src/main/kotlin/de/unisaarland/cs/se/selab/Main.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Main.kt
@@ -1,7 +1,46 @@
 package de.unisaarland.cs.se.selab
 
+import de.unisaarland.cs.se.selab.parsing.SimulationParser
+import de.unisaarland.cs.se.selab.logger.Logger
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
+import java.io.FileOutputStream
+import java.io.PrintWriter
+
+/**
+ * Entry point of the simulation.
+ */
 fun main(args: Array<String>) {
-    throw IllegalStateException("Implement Me!.")
+    val argsParser = ArgParser("Save the Ocean!")
+    val map by argsParser.option(ArgType.String, "map").required()
+    val corporations by argsParser.option(ArgType.String, "assets").required()
+    val scenario by argsParser.option(ArgType.String, "scenario").required()
+    val maxTick by argsParser.option(ArgType.Int, "ticks").required()
+    val output by argsParser.option(ArgType.String, "out")
+
+    argsParser.parse(args)
+
+    // handle case when no output file is provided.
+    val outputStream = if (output == null) {
+        System.out
+    } else {
+        // if output not null, call FileOutputStream(output)
+        output?.let { FileOutputStream(it) }
+    }
+
+    // init logger with output
+    outputStream?.let { PrintWriter(it) }?.let { Logger.setOutput(it) }
+
+    // Create SimParser
+    val simParser = SimulationParser(map, corporations, scenario, maxTick)
+
+    // Parse Files and create simulator
+    val sim = simParser.createSimulator()
+
+    // run -- add sim.run or whatever
+    TODO()
 }
+
 
 
