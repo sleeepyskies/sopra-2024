4d54a144bd594e22faeddf293610eff39c4a8390
Sky Waller <skyswalla@gmail.com>
2024-09-12 22:31:03 +0200

    made mapparse more beautiful

--- src/main/kotlin/de/unisaarland/cs/se/selab/parsing/MapParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parsing/MapParser.kt
@@ -1,13 +1,17 @@
 
 package de.unisaarland.cs.se.selab.parsing
-import com.github.erosb.jsonsKema.*
 import de.unisaarland.cs.se.selab.assets.Current
 import de.unisaarland.cs.se.selab.assets.Direction
 import de.unisaarland.cs.se.selab.assets.Tile
 import de.unisaarland.cs.se.selab.assets.TileType
 import de.unisaarland.cs.se.selab.navigation.NavigationManager
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 import java.io.File
+import java.io.IOException
 
 /**
  * Parses map data from a file.
@@ -18,15 +22,22 @@ import java.io.File
 class MapParser(
     private val mapFilePath: String
 ) {
+    // debug logger
+    private val log: Log = LogFactory.getLog("debugger")
+
+    // parser helper
+    private val helper = ParserHelper()
+
     // schemas
     private val mapSchema = "map.schema"
     private val tileSchema = "tile.schema"
 
-    // validator
-    private var validator = initMapSchemaValidator()
-
     // Map data
     private val map: MutableMap<Pair<Int, Int>, Tile> = mutableMapOf()
+
+    // used for validation
+    private val tileIDs = mutableListOf<Int>()
+    private val tileLocs = mutableListOf<Pair<Int, Int>>()
 
     // Map of tile id to location used in corporation parser
     val idLocationMapping: MutableMap<Int, Pair<Int, Int>> = mutableMapOf()
@@ -36,70 +47,131 @@ class MapParser(
      *
      * @return True if the parsing was successful, false otherwise.
      */
-    public fun parseMap(): Boolean {
+    fun parseMap(): Boolean {
+        // success variable
+        var success = true
 
-        // parse input file
-        val mapFileContent = File(mapFilePath).readText()
-        val mapJSON = JsonParser(mapFileContent).parse()
-
-        // validate map file according to schema
-        // still need to properly handle this
-        val schemaValidation = validator.validate(mapJSON)
-
-        // create JSONObject from file
-        val mapJSONObject = JSONObject(File(mapFilePath).readText())
-
-        // Get Tile array from file
-        val tilesJSON = mapJSONObject.getJSONArray("tiles")
-
-        // parse and validate all tiles
-        for (index in 0 until tilesJSON.length()) {
-            parseTile(tilesJSON.getJSONObject(index))
+        // create map JSON object
+        val mapJSONObject = try {
+            JSONObject(File(mapFilePath).readText())
+        } catch (e: IOException) {
+            log.error("MAP PARSER: The file could not be read.", e)
+            return false
+        } catch (e: JSONException) {
+            log.error("MAP PARSER: The file is not a valid JSON.", e)
+            return false
         }
 
+        // validate map JSON against schema
+        if (!helper.validateSchema(mapJSONObject, this.mapSchema)) {
+            log.error("MAP PARSER: The file does not match the schema.")
+            success = false
+        }
+
+        // Get Tile array from file
+        val tileJSONArray = mapJSONObject.getJSONArray("tiles")
+
+        // parse and validate all tiles
+        success = success && parseTiles(tileJSONArray)
+
         // validate the map
-        validateMapProperties()
+        success = success && validateMapProperties()
+
+        return true
+    }
+
+    /**
+     * Iterates over a JSONArray and parses all tiles.
+     * @return true if parsing was a success, false otherwise.
+     */
+    private fun parseTiles(tileJSONArray: JSONArray): Boolean {
+        for (index in 0 until tileJSONArray.length()) {
+            // get tile JSON
+            val tileJSON = tileJSONArray.getJSONObject(index)
+
+            // validate garbage JSON against schema
+            if (!helper.validateSchema(tileJSON, this.tileSchema)) {
+                log.error("MAP PARSER: The tile do not match the schema.")
+                return false
+            }
+
+            // get tile
+            val tile = parseTile(tileJSON)
+
+            // check garbage is valid and created correctly
+            if (tile == null || !validateTileProperties(tile)) {
+                log.error(
+                    "MAP PARSER: A tile does not have a unique ID" +
+                        " or could not be correctly instantiated."
+                )
+                return false
+            }
+
+            // add to map
+            this.map[tile.location] = tile
+
+            // add validation data
+            this.idLocationMapping[tile.id] = tile.location
+            this.tileIDs.add(tile.id)
+            this.tileLocs.add(tile.location)
+        }
 
         return true
     }
 
     /**
      * Parses a tile from a string and adds it to the map data structure
+     * @return the object if parsed correctly, null otherwise.
      */
-    private fun parseTile(tileJSON: JSONObject) {
-        // validate tile according to schema file
-        val tileValidator = makeTileSchemaValidator()
-        val tileJSONValue = JsonParser(tileJSON.toString()).parse()
-        val parseResult = tileValidator.validate(tileJSONValue)
-
+    private fun parseTile(tileJSON: JSONObject): Tile? {
         // get values
         val id = tileJSON.getInt("id")
         val coordinatesJSON = tileJSON.getJSONObject("coordinates")
-        val coordX = coordinatesJSON.getInt("x")
-        val coordY = coordinatesJSON.getInt("y")
-        val category = makeCategory(tileJSON.getString("category"))
-        val harbor = tileJSON.getBoolean("harbor")
-        // for current
-        val current = tileJSON.getBoolean("current")
-        val direction = makeDirection(tileJSON.getInt("direction"))
-        val speed = tileJSON.getInt("speed")
-        val intensity = tileJSON.getInt("intensity")
+        val cordX = coordinatesJSON.getInt("x")
+        val cordY = coordinatesJSON.getInt("y")
+        val category = helper.makeTileType(tileJSON.getString("category"))
 
-        if (
-            validateTileProperties(id, coordX, coordY, category, harbor, current, direction, speed, intensity) &&
-            category != null &&
-            direction != null
-        ) {
-            val tileCurrent = Current(direction, intensity, speed)
-            // add to map structure
-            this.map[Pair(coordX, coordY)] = Tile(id, Pair(coordX, coordY), category, harbor, tileCurrent, current)
-            // add to id location mapping for corporation parser
-            this.idLocationMapping[id] = Pair(coordX, coordY)
+        // useless current *spits*
+        val mockCurrent = Current(Direction.EAST, 0, 0)
+
+        // handle different tile types
+        return when (category) {
+            TileType.LAND -> {
+                Tile(id, Pair(cordX, cordY), TileType.LAND, false, mockCurrent, false, 0)
+            }
+            TileType.SHALLOW_OCEAN -> {
+                Tile(id, Pair(cordX, cordY), TileType.SHALLOW_OCEAN, false, mockCurrent, false)
+            }
+            TileType.DEEP_OCEAN -> {
+                val current = tileJSON.getBoolean("current")
+                if (current) {
+                    // has current
+                    val direction = helper.makeDirection(tileJSON.getInt("direction"))
+                    val speed = tileJSON.getInt("speed")
+                    val intensity = tileJSON.getInt("intensity")
+                    val realCurrent = direction?.let { Current(it, intensity, speed) }
+                    if (realCurrent != null) {
+                        Tile(id, Pair(cordX, cordY), TileType.SHALLOW_OCEAN, false, realCurrent, true)
+                    } else {
+                        null
+                    }
+                } else {
+                    // has no current
+                    Tile(id, Pair(cordX, cordY), TileType.SHALLOW_OCEAN, false, mockCurrent, false)
+                }
+            }
+            TileType.SHORE -> {
+                val harbor = tileJSON.getBoolean("harbor")
+                Tile(id, Pair(cordX, cordY), TileType.SHORE, harbor, mockCurrent, false, 0)
+            }
+            else -> {
+                null
+            }
         }
     }
 
     /**
-     * Validates the parameters in the map.
+     * Checks if each map tile is correctly placed next to other tiles.
      *
      * @return True if valid, false otherwise
      */
@@ -112,74 +184,15 @@ class MapParser(
      *
      * @return True if valid, false otherwise
      */
-    private fun validateTileProperties (
-        id: Int,
-        coordX: Int,
-        coordY: Int,
-        categoryString: TileType?,
-        harbor: Boolean,
-        current: Boolean,
-        direction: Direction?,
-        speed: Int,
-        intensity: Int
-    ): Boolean {
-        return true
+    private fun validateTileProperties(tile: Tile): Boolean {
+        // check ID is unique and location is unique
+        return !this.tileIDs.contains(tile.id) && !this.tileLocs.contains(tile.location)
     }
 
     /**
      * Returns the parsed NavigationManager.
      */
-    public fun getNavManager(): NavigationManager {
+    fun getNavManager(): NavigationManager {
         return NavigationManager(map)
-    }
-
-    /**
-     * Returns a new schema validator using the tile schema
-     */
-    private fun makeTileSchemaValidator(): Validator {
-        // read content of JSON schema
-        val schemaContent = File(tileSchema).readText()
-        // Parse JSON schema as string
-        val schemaJSON = JsonParser(schemaContent).parse()
-        // create schema instance
-        val schema = SchemaLoader(schemaJSON).load()
-        // create and return validator
-        return Validator.create(schema, ValidatorConfig(FormatValidationPolicy.ALWAYS))
-    }
-
-    /**
-     * Sets up the map schema validator.
-     */
-    private fun initMapSchemaValidator(): Validator {
-        // read content of JSON schema
-        val schemaContent = File(mapSchema).readText()
-        // Parse JSON schema as string
-        val schemaJSON = JsonParser(schemaContent).parse()
-        // create schema instance
-        val schema = SchemaLoader(schemaJSON).load()
-        // create and return validator
-        return Validator.create(schema, ValidatorConfig(FormatValidationPolicy.ALWAYS))
-    }
-
-    private fun makeCategory(categoryString: String): TileType? {
-        return when (categoryString) {
-            "LAND" -> TileType.LAND
-            "SHORE" -> TileType.SHORE
-            "SHALLOW_OCEAN" -> TileType.SHALLOW_OCEAN
-            "DEEP_OCEAN" -> TileType.DEEP_OCEAN
-            else -> null
-        }
-    }
-
-    private fun makeDirection(direction: Int): Direction? {
-        return when (direction) {
-            0 -> Direction.EAST
-            60 -> Direction.SOUTH_EAST
-            120 -> Direction.SOUTH_WEST
-            180 -> Direction.WEST
-            240 -> Direction.NORTH_WEST
-            300 -> Direction.NORTH_EAST
-            else -> null
-        }
     }
 }
