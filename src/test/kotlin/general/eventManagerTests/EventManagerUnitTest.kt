41fa61b8f9b666813a6406ecd0aef002c78e7719
ogak00001 <ogak00001@stud.uni-saarland.de>
2024-09-17 12:25:32 +0200

    cleaned up warnings

--- src/test/kotlin/general/eventManagerTests/EventManagerUnitTest.kt
+++ src/test/kotlin/general/eventManagerTests/EventManagerUnitTest.kt
@@ -43,49 +43,49 @@ class EventManagerUnitTest {
     }
 
     // mock current for tiles with no current
-    val current = Current(Direction.EAST, 0, 0)
+    private val current = Current(Direction.EAST, 0, 0)
 
     // current for t1
-    val currentT1 = Current(Direction.SOUTH_EAST, 5, 1)
+    private val currentT1 = Current(Direction.SOUTH_EAST, 5, 1)
 
     // current for t6
-    val currentT6 = Current(Direction.EAST, 5, 3)
+    private val currentT6 = Current(Direction.EAST, 5, 3)
 
     // row 0
-    val t1 = Tile(0, Pair(0, 0), TileType.DEEP_OCEAN, false, currentT1, true, 1000)
-    val t2 = Tile(1, Pair(1, 0), TileType.SHALLOW_OCEAN, false, current, false, 1000)
-    val t3 = Tile(2, Pair(2, 0), TileType.SHORE, false, current, false, 1000)
-    val t4 = Tile(3, Pair(3, 0), TileType.SHORE, false, current, false, 1000)
-    val t5 = Tile(4, Pair(4, 0), TileType.SHORE, true, current, false, 1000)
+    private val t1 = Tile(0, Pair(0, 0), TileType.DEEP_OCEAN, false, currentT1, true, 1000)
+    private val t2 = Tile(1, Pair(1, 0), TileType.SHALLOW_OCEAN, false, current, false, 1000)
+    private val t3 = Tile(2, Pair(2, 0), TileType.SHORE, false, current, false, 1000)
+    private val t4 = Tile(3, Pair(3, 0), TileType.SHORE, false, current, false, 1000)
+    private val t5 = Tile(4, Pair(4, 0), TileType.SHORE, true, current, false, 1000)
 
     // row 1
-    val t6 = Tile(5, Pair(0, 1), TileType.DEEP_OCEAN, false, currentT6, true, 1000)
-    val t7 = Tile(6, Pair(1, 1), TileType.SHALLOW_OCEAN, false, current, false, 1000)
-    val t8 = Tile(7, Pair(2, 1), TileType.SHORE, false, current, false, 1000)
-    val t9 = Tile(8, Pair(3, 1), TileType.LAND, false, current, false, 1000)
-    val t10 = Tile(9, Pair(4, 1), TileType.LAND, false, current, false, 1000)
+    private val t6 = Tile(5, Pair(0, 1), TileType.DEEP_OCEAN, false, currentT6, true, 1000)
+    private val t7 = Tile(6, Pair(1, 1), TileType.SHALLOW_OCEAN, false, current, false, 1000)
+    private val t8 = Tile(7, Pair(2, 1), TileType.SHORE, false, current, false, 1000)
+    private val t9 = Tile(8, Pair(3, 1), TileType.LAND, false, current, false, 1000)
+    private val t10 = Tile(9, Pair(4, 1), TileType.LAND, false, current, false, 1000)
 
     // row 2
-    val t11 = Tile(10, Pair(0, 2), TileType.SHALLOW_OCEAN, false, current, false, 1000)
-    val t12 = Tile(11, Pair(1, 2), TileType.SHORE, false, current, false, 1000)
-    val t13 = Tile(12, Pair(2, 2), TileType.LAND, false, current, false, 1000)
-    val t14 = Tile(13, Pair(3, 2), TileType.SHORE, false, current, false, 1000)
-    val t15 = Tile(14, Pair(4, 2), TileType.SHORE, true, current, false, 1000)
+    private val t11 = Tile(10, Pair(0, 2), TileType.SHALLOW_OCEAN, false, current, false, 1000)
+    private val t12 = Tile(11, Pair(1, 2), TileType.SHORE, false, current, false, 1000)
+    private val t13 = Tile(12, Pair(2, 2), TileType.LAND, false, current, false, 1000)
+    private val t14 = Tile(13, Pair(3, 2), TileType.SHORE, false, current, false, 1000)
+    private val t15 = Tile(14, Pair(4, 2), TileType.SHORE, true, current, false, 1000)
 
     // row 3
-    val t16 = Tile(15, Pair(0, 3), TileType.SHALLOW_OCEAN, false, current, false, 1000)
-    val t17 = Tile(16, Pair(1, 3), TileType.SHORE, false, current, false, 1000)
-    val t18 = Tile(17, Pair(2, 3), TileType.LAND, false, current, false, 1000)
-    val t19 = Tile(18, Pair(3, 3), TileType.SHORE, false, current, false, 1000)
-    val t20 = Tile(19, Pair(4, 3), TileType.LAND, false, current, false, 1000)
+    private val t16 = Tile(15, Pair(0, 3), TileType.SHALLOW_OCEAN, false, current, false, 1000)
+    private val t17 = Tile(16, Pair(1, 3), TileType.SHORE, false, current, false, 1000)
+    private val t18 = Tile(17, Pair(2, 3), TileType.LAND, false, current, false, 1000)
+    private val t19 = Tile(18, Pair(3, 3), TileType.SHORE, false, current, false, 1000)
+    private val t20 = Tile(19, Pair(4, 3), TileType.LAND, false, current, false, 1000)
 
     // row 4
-    val t21 = Tile(20, Pair(0, 4), TileType.SHORE, false, current, false, 1000)
-    val t22 = Tile(21, Pair(1, 4), TileType.LAND, false, current, false, 1000)
-    val t23 = Tile(22, Pair(2, 4), TileType.SHORE, false, current, false, 1000)
-    val t24 = Tile(23, Pair(3, 4), TileType.LAND, false, current, false, 1000)
-    val t25 = Tile(24, Pair(4, 4), TileType.SHORE, false, current, false, 1000)
-    val map = mapOf(
+    private val t21 = Tile(20, Pair(0, 4), TileType.SHORE, false, current, false, 1000)
+    private val t22 = Tile(21, Pair(1, 4), TileType.LAND, false, current, false, 1000)
+    private val t23 = Tile(22, Pair(2, 4), TileType.SHORE, false, current, false, 1000)
+    private val t24 = Tile(23, Pair(3, 4), TileType.LAND, false, current, false, 1000)
+    private val t25 = Tile(24, Pair(4, 4), TileType.SHORE, false, current, false, 1000)
+    private val map = mapOf(
         // row 0
         Pair(0, 0) to t1,
         Pair(1, 0) to t2,
@@ -132,7 +132,7 @@ class EventManagerUnitTest {
             1, "black_pearl", 1, mutableMapOf(), 10, Pair(0, 0),
             Direction.EAST, 1, 10, 10, 10, 1000,
             10, 1000, -1, ShipState.DEFAULT, ShipType.SCOUTING_SHIP,
-            false, false, false
+            hasRadio = false, hasTracker = false, travelingToHarbor = false
         )
         val corp = mock<Corporation> {
             on { id } doReturn 1
