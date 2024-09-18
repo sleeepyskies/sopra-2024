19dfc2114b33cb38a65750e242dc46b63b60d449
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2024-09-18 13:54:59 +0200

    added cross validation to simulation parser

--- src/main/kotlin/de/unisaarland/cs/se/selab/parsing/SimulationParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parsing/SimulationParser.kt
@@ -469,6 +469,7 @@ class SimulationParser(
             crossValidateRewardAssignedIds() &&
             crossValidateRewardGarbageTypeOnTile()
     }
+
     private fun crossValidateRewardGarbageTypeOnTile(): Boolean {
         // get all reward initial locations
         for (task in this.tasks.flatMap { it.value }.filter { it.type == TaskType.COLLECT }) {
