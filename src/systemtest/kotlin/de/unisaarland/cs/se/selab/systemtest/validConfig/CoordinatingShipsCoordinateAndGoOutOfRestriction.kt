92152bedb8b11350fcc76525c79cc1bcf6d8f0ab
Jona-yazdan Prinz <jona.yazdan.prinz@gmail.com>
2024-09-19 11:02:11 +0200

    finished CoordinatingShipsTest

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/CoordinatingShipsCoordinateAndGoOutOfRestriction.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/CoordinatingShipsCoordinateAndGoOutOfRestriction.kt
@@ -1,43 +1,47 @@
 package de.unisaarland.cs.se.selab.systemtest.validConfig
 
-import de.unisaarland.cs.se.selab.systemtest.api.SystemTestAssertionError
 import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
-import de.unisaarland.cs.se.selab.systemtest.utils.Logs
 
 /**
  * applying pirate attack and checking if ship was removed
  */
 class CoordinatingShipsCoordinateAndGoOutOfRestriction : ExampleSystemTestExtension() {
     override val description = "testing to see of ships coordinate coorectly, and go out of the restriction correctly" +
-            "with correct accelerations"
+        "with correct accelerations"
     override val corporations = "corporationJsons/corpWithTwoCoordinatingShips.json"
     override val scenario = "scenarioJsons/restrictionAndOilSpillPlusCooperatingTask.json"
     override val map = "mapFiles/smallMap1.json"
     override val name = "test"
     override val maxTicks = 4
     override suspend fun run() {
-        val expectedString = "Event: Event 1 of type PIRATE_ATTACK happened."
+        initSimulation()
+        tick0()
+        tick1()
+        tick2()
+        tick3()
+        simEnd()
     }
+
+    private suspend fun initSimulation() {
+        skipLines(4)
+    }
+
     private suspend fun tick0() {
         assertNextLine("Simulation Info: Tick 0 started.")
 
         // Corporation Phase 1
-        assertNextLine("Corporation Action: Corporation 1 is starting to move its ships.")
+        skipLines(1)
         assertNextLine("Ship Movement: Ship 1 moved with speed 15 to tile 7.")
-        assertNextLine("Corporation Action: Corporation 1 is starting to collect garbage.")
-        assertNextLine("Corporation Action: Corporation 1 is starting to cooperate with other corporations.")
-        assertNextLine("Corporation 1 cooperated with corporation 2 with ship 1 to ship 2.")
-        assertNextLine("Corporation Action: Corporation 1 is starting to refuel.")
-        assertNextLine("Corporation Action: Corporation 1 finished its actions.")
+        skipLines(2)
+        assertNextLine("Cooperation: Corporation 1 cooperated with corporation 2 with ship 1 to ship 2.")
+        skipLines(2)
 
         // Corporation Phase 2
-        assertNextLine("Corporation Action: Corporation 2 is starting to move its ships.")
+        skipLines(1)
         assertNextLine("Corporation Action: Corporation 2 is starting to collect garbage.")
         assertNextLine("Corporation Action: Corporation 2 is starting to cooperate with other corporations.")
-        assertNextLine("Corporation 2 cooperated with corporation 1 with ship 2 to ship 1.")
-        assertNextLine("Corporation Action: Corporation 1 is starting to refuel.")
-        assertNextLine("Corporation Action: Corporation 1 finished its actions.")
-
+        assertNextLine("Cooperation: Corporation 2 cooperated with corporation 1 with ship 2 to ship 1.")
+        skipLines(2)
     }
 
     private suspend fun tick1() {
@@ -46,5 +50,42 @@ class CoordinatingShipsCoordinateAndGoOutOfRestriction : ExampleSystemTestExtens
         skipLines(10) // No Ships move
 
         assertNextLine("Event: Event 1 of type RESTRICTION happened.")
+    }
+
+    private suspend fun tick2() {
+        assertNextLine("Simulation Info: Tick 2 started.")
+        assertNextLine("Corporation Action: Corporation 1 is starting to move its ships.")
+        assertNextLine("Ship Movement: Ship 1 moved with speed 15 to tile 5.")
+        skipLines(1)
+        assertNextLine("Corporation Action: Corporation 1 is starting to cooperate with other corporations.")
+        skipLines(3)
+        assertNextLine("Ship Movement: Ship 2 moved with speed 10 to tile 5.")
+        skipLines(4)
+        assertNextLine("Task: Task 1 of type COORDINATE with ship 1 is added with destination 9.")
+    }
+
+    private suspend fun tick3() {
+        assertNextLine("Simulation Info: Tick 3 started.")
+        assertNextLine("Corporation Action: Corporation 1 is starting to move its ships.")
+        assertNextLine("Ship Movement: Ship 1 moved with speed 30 to tile 9.")
+        assertNextLine("Corporation Action: Corporation 1 is starting to collect garbage.")
+        assertNextLine("Corporation Action: Corporation 1 is starting to cooperate with other corporations.")
+        assertNextLine("Corporation Action: Corporation 1 is starting to refuel.")
+        assertNextLine("Corporation Action: Corporation 1 finished its actions.")
+        assertNextLine("Corporation Action: Corporation 2 is starting to move its ships.")
+        assertNextLine("Ship Movement: Ship 2 moved with speed 20 to tile 3.")
+        skipLines(4)
+        assertNextLine("Reward: Task 1: Ship 1 received reward of type RADIO.")
+    }
+    private suspend fun simEnd() {
+        assertNextLine("Simulation Info: Simulation ended.")
+        assertNextLine("Simulation Info: Simulation statistics are calculated.")
+        assertNextLine("Simulation Statistics: Corporation 1 collected 0 of garbage.")
+        assertNextLine("Simulation Statistics: Corporation 2 collected 0 of garbage.")
+        assertNextLine("Simulation Statistics: Total amount of plastic collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of oil collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of chemicals collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of garbage still in the ocean: 0.")
+        assertEnd()
     }
 }
