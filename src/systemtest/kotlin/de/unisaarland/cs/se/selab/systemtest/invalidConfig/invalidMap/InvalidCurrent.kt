a06977dd805d6fc573fca51213fc0e111e03dc39
skysw <skwa00001@stud.uni-saarland.de>
2024-09-19 12:53:15 +0200

    typos

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/invalidConfig/invalidMap/InvalidCurrent.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/invalidConfig/invalidMap/InvalidCurrent.kt
@@ -3,17 +3,17 @@ package de.unisaarland.cs.se.selab.systemtest.invalidConfig.invalidMap
 import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
 
 /**
- * applying pirate attack and checking if ship was removed
+ * Current with 1000 speed.
  */
-class InavlidCurrent : ExampleSystemTestExtension() {
+class InvalidCurrent : ExampleSystemTestExtension() {
     override val description = "Current with 1000 speed"
     override val corporations = "corporationJsons/corpWithTwoShips.json"
     override val scenario = "scenarioJsons/restrictionAndOilSpillPlusCooperatingTask.json"
-    override val map = "mapFiles/validationFiles/invalidFiles/obamnaWrongCurretn.json"
+    override val map = "mapFiles/validationFiles/invalidFiles/obamnaInvalidCurrent.json"
     override val name = "test"
     override val maxTicks = 0
     override suspend fun run() {
-        assertNextLine("Initialization Info: obamnaWrongCuretn.json is invalid.")
+        assertNextLine("Initialization Info: obamnaInvalidCurrent.json is invalid.")
         assertEnd()
     }
 }
