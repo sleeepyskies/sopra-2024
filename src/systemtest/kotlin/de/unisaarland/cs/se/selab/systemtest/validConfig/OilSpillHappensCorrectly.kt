9de0eb69458d26c9f01c486bbe975f8fcd44d753
Jona-yazdan Prinz <jona.yazdan.prinz@gmail.com>
2024-09-23 00:52:38 +0200

    Temporary commit, changing OilSpillHappensCorrectly

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/OilSpillHappensCorrectly.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/OilSpillHappensCorrectly.kt
@@ -9,16 +9,50 @@ import de.unisaarland.cs.se.selab.systemtest.utils.Logs
  */
 class OilSpillHappensCorrectly : ExampleSystemTestExtension() {
     override val description = "applying oil spill"
-    override val corporations = "corporationJsons/empty_corps.json"
+    override val corporations = "corporationJsons/scoutingShipForOilSpillCollect.json"
     override val scenario = "scenarioJsons/justOilSpill.json"
     override val map = "mapFiles/obamna.json"
     override val name = "OilSpillCorrect"
-    override val maxTicks = 2
+    override val maxTicks = 5
     override suspend fun run() {
-        val expectedString = "Simulation Statistics: Total amount of garbage still in the ocean: 7000."
-        if (skipUntilLogType(Logs.TOTAL_AMOUNT_OF_GARBAGE) != expectedString) {
-            throw SystemTestAssertionError("Uncollected oil should be 7000!")
-        }
+        /*initSimulation()
+        tick0()
+        tick1()
+        tick2()
+        simEnd()*/
+    }
+
+    private suspend fun initSimulation() {
+        skipLines(4)
+    }
+
+    private suspend fun tick0() {
+        assertNextLine("Simulation Info: Tick 0 started.")
+
+        // Corporation Phase 1
+        skipLines(5)
+        assertNextLine("Current Drift: OIL 75 with amount 50 drifted from tile 1 to tile 1.")
+        assertNextLine("Current Drift: OIL 76 with amount 150 drifted from tile 6 to tile 8.")
+    }
+
+    private suspend fun tick1() {
+        assertNextLine("Simulation Info: Tick 1 started.")
+
+        skipLines(5) // No Ships move
+        assertNextLine("Current Drift: OIL 77 with amount 50 drifted from tile 1 to tile 1.")
+    }
+
+    private suspend fun tick2() {
+        skipLines(6)
+    }
+    private suspend fun simEnd() {
+        assertNextLine("Simulation Info: Simulation ended.")
+        assertNextLine("Simulation Info: Simulation statistics are calculated.")
+        assertNextLine("Simulation Statistics: Corporation 1 collected 0 of garbage.")
+        assertNextLine("Simulation Statistics: Total amount of plastic collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of oil collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of chemicals collected: 0.")
+        assertNextLine("Simulation Statistics: Total amount of garbage still in the ocean: 2300.")
         assertEnd()
     }
 }
