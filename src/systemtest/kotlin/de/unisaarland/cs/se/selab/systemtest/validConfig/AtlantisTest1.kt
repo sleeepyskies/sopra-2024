eaef86c5623cbebc68080ccc93c8f5c81b69cd2c
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2024-09-24 18:35:59 +0200

    mutant test

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/AtlantisTest1.kt
@@ -0,0 +1,16 @@
+package de.unisaarland.cs.se.selab.systemtest.validConfig
+
+import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
+
+class AtlantisTest1 : ExampleSystemTestExtension() {
+    override val description = "no"
+    override val corporations = "corporationJsons/atlantis1Corp.json"
+    override val scenario = "scenarioJsons/noWayOutOfRestriction.json"
+    override val map = "mapFiles/atlantis1Map.json"
+    override val name = "NoWayToLeaveRestriction: task should be assigned even if ship capacity is full"
+    override val maxTicks = 5
+
+    override suspend fun run() {
+        assertNextLine("Initialization Info: atlantis1Map.json is invalid.")
+    }
+}
