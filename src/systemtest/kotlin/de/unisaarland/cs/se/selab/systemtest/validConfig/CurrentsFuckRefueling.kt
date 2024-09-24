4f79c5dc45972217fce2a98ceed68fe04371d339
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2024-09-24 14:56:19 +0200

    system yesy

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/CurrentsFuckRefueling.kt
@@ -0,0 +1,20 @@
+package de.unisaarland.cs.se.selab.systemtest.validConfig
+
+import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
+/** checks when refueling is not possible due to currents */
+class CurrentsFuckRefueling : ExampleSystemTestExtension() {
+    override val description = "no"
+    override val corporations = "corporationJsons/currentsFuckRefuelingCorp.json"
+    override val scenario = "scenarioJsons/currentsFuckRefuelingScenario.json"
+
+    override val map = "mapFiles/bigMap1.json"
+    override val name = "task should be assigned even if ship capacity is full"
+    override val maxTicks = 10
+
+    override suspend fun run() {
+        initSimulation()
+    }
+    suspend fun initSimulation() {
+        skipLines(4)
+    }
+}
