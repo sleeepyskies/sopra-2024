8386ecef9143703397e2158861c64165d7a0eeb2
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2024-09-25 15:47:44 +0200

    mutantz

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/MultiReward.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/MultiReward.kt
@@ -4,13 +4,16 @@ import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
 /**tesst**/
 class MultiReward : ExampleSystemTestExtension() {
     override val description = "no"
-    override val corporations = "corporationJsons/MultiReward.json"
+    override val corporations = "corporationJsons/MultiReward_corp.json"
     override val scenario = "scenarioJsons/MultiReward.json"
     override val map = "mapFiles/smallMap1.json"
     override val name = "MultiReward"
     override val maxTicks = 9
 
     override suspend fun run() {
-        skipLines(1)
+        assertNextLine("Initialization Info: smallMap1.json successfully parsed and validated.")
+        assertNextLine("Initialization Info: MultiReward_corp.json successfully parsed and validated.")
+        assertNextLine("Initialization Info: MultiReward.json is invalid.")
+        assertEnd()
     }
 }
