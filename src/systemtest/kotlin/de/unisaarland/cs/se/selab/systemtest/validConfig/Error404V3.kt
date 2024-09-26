e243f90b3f8459d99de085854baba67fd1ce1721
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2024-09-26 10:30:23 +0200

    updated the names of the tests.

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/Error404V3.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/Error404V3.kt
@@ -3,7 +3,7 @@ package de.unisaarland.cs.se.selab.systemtest.validConfig
 import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
 /** asda **/
 class Error404V3 : ExampleSystemTestExtension() {
-    override val description = "no"
+    override val description = "ship doesnt belong to any corp"
     override val corporations = "corporationJsons/shipWithNonExistingCorp.json"
     override val scenario = "scenarioJsons/empty_scen.json"
     override val map = "mapFiles/smallMap1.json"
--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/Error404V4.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/validConfig/Error404V4.kt
@@ -3,7 +3,7 @@ package de.unisaarland.cs.se.selab.systemtest.validConfig
 import de.unisaarland.cs.se.selab.systemtest.utils.ExampleSystemTestExtension
 /**ja**/
 class Error404V4 : ExampleSystemTestExtension() {
-    override val description = "no"
+    override val description = "garbage types of corporation not collected by any ship"
     override val corporations = "corporationJsons/wrongGBTypes.json"
     override val scenario = "scenarioJsons/empty_scen.json"
     override val map = "mapFiles/smallMap1.json"
